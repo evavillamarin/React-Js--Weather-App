7fe2b8b666c79aff61e7c7191e23ab63f818542b
evavillamarin <emvillamarin09@gmail.com>
2025-08-31 12:40:47 +0800

    Added New Feature in Weather.jsx

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -30,10 +30,21 @@ function Weather() {
 
   }
   const search = async(city)=> {
+    if(city === "") {
+        alert("Enter City Name");
+        return;
+    }
       try {
           const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`;
           const response = await fetch (url);
           const data = await response.json();
+
+          if(!response.ok){
+            alert(data.message);
+            return;
+          }
+
+
           console.log(data); 
           const icon = allIcons[data.weather[0].icon] || clear_icon;
           setWeatherData({
@@ -44,7 +55,8 @@ function Weather() {
             icon: icon
           })
         } catch (error){
-
+            setWeatherData(false);
+            console.error("Error in Fetching Waether Data");
       }
 
   }
@@ -58,6 +70,7 @@ function Weather() {
             <input type='text' ref={inputRef} placeholder='Search'/>
             <img src={search_icon} onClick={()=>search(inputRef.current.value)} alt=''/>
         </div>
+        {weatherData?<>
         <img src={weatherData.icon} alt="" className='weather-icon'/>
         <p className='temperature'>{weatherData.temperature}°c</p>
         <p className='location'>{weatherData.location}</p>
@@ -76,7 +89,7 @@ function Weather() {
                   <span>Wind Speed</span>
               </div>
             </div>    
-        </div>
+        </div></>:<></>}
         <div className='developedby'>
           <p>Developed by <span>EMDev</span></p>
         </div>
